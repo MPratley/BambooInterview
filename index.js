dd13077a8d6678cd1a08630490b742bd11054fd6
Marcus Pratley <marcuspratley@gmail.com>
2020-07-18 19:24:39 +0100

    static file serving

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 'use strict'
 
 const Koa = require('koa')
+const mount = require('koa-mount')
+const serve = require('koa-static')
 
 const app = new Koa()
+
+// statically serve assets
+app.use(mount('/', serve('./static')))
 
 app.use(async ctx => {
   ctx.body = 'Hello World'
 })
 
-app.listen(3000)
+app.listen(3000, () => console.log('running on port 3000, http://localhost:3000'))
