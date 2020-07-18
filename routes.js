3b9fb31164c9f47c3214b8ea0b79ba99cfa6025f
Marcus Pratley <marcuspratley@gmail.com>
2020-07-18 23:14:28 +0100

    redirect to login

--- routes.js
+++ routes.js
@@ -4,11 +4,13 @@ const app = require('./index.js').app
 const Router = require('koa-router')
 const router = new Router()
 
-router.get('/', async (ctx) => {
-  await ctx.render('index', {
+router.get('/login', async (ctx) => {
+  await ctx.render('login', {
     title: 'Bambank'
   })
 })
+
+router.redirect('/', '/login')
 
 app.use(router.routes())
 app.use(router.allowedMethods())
