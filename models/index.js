b213df7efd4859d681262ef676f5377eeffc7fe8
Marcus Pratley <marcuspratley@gmail.com>
2020-07-20 15:30:13 +0100

    promotional hook

--- models/index.js
+++ models/index.js
@@ -21,6 +21,10 @@ db.transfer.belongsTo(db.user, { as: 'sender' })
 db.transfer.belongsTo(db.user, { as: 'receiver' })
 db.user.belongsToMany(db.transfer, { through: 'usertransfers' })
 
+// Extra Hooks
+db.user.addHook('afterCreate', (user) => {
+  db.transfer.create({ balance: 100, message: 'Welcome to Bambank! This is a complimentary 100 Bambeuros to get you started', receiverIdentifier: user.identifier })
+})
 db.sequelize = sequelize
 db.Sequelize = Sequelize
 db.Op = Op
